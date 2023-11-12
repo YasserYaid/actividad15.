a9cf06102e0b17c3201407227e2ae030f2cbc92a
YasserYaid <zs19014018@estudiantes.uv.mx>
2023-11-12 13:50:36 -0600

    Agregar archivos de proyecto.

---
+++ CorreoFei/Models/ContactoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CorreoFei.Models
+{
+    public class ContactoViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Nombre del contacto")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El campo {0} no es una direccion de correo electronica valida.")]
+        [Display(Name ="Correo electronico")]
+        public string Correo { get; set; }
+    }
+}
---
+++ CorreoFei/Services/ErrorLog/ErrorLog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace CorreoFei.Services.ErrorLog
+{
+    public class ErrorLog : IErrorLog
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpContextAccessor _httocontextAccessor;
+
+        public ErrorLog(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httocontextAccessor)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _httocontextAccessor = httocontextAccessor;
+        }
+
+        [HttpPost]
+        public async Task ErrorLogAsync(string Mensaje)
+        {
+            try
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                string path = "";
+                path = Path.Combine(webRootPath, "log");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                //Retrieve server/local IP address
+                var feature = _httocontextAccessor.HttpContext.Features.Get<IHttpConnectionFeature>();
+                string LocalIPAddr = feature?.LocalIpAddress?.ToString();
+                //Write the especified text asynchronously to a new file named "WriteTextAsync.txt"
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "log.txt"), true))
+                {
+                    await outputFile.WriteLineAsync(Mensaje + " - " +
+                        _httocontextAccessor.HttpContext.User.Identity.Name +
+                        " - " + LocalIPAddr + " - " + DateTime.Now.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                //No hace nada
+            }
+        }
+    }
+}
